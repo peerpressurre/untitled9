0fe0831bd620d5fcecb4099ed7797add93c47579
student <popihacvova@gmail.com>
2022-11-05 13:37:28 +0200

    add

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 import statistics
 def pali():
    try:
-        list = []
-        print('To break ')
-        text = 0
-        while True:
-                text = int(input('text->'))
-                if text > 0:
-                    list.append(text)
-                else:
-                    break
+       num = int(input('num->'))
+       for i in range(num, 0):
+           
 
-
-        summary = sum(list)
-        length = len(list)
-        avr = summary / length
-        print(avr)
    except Exception as ex:
         print(f'Error: {ex}')
 pali()
